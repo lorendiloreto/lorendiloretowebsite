d3877b1d3eda017d79f900563955a0d20383739f
lorendiloreto <lorendiloreto@live.com>
2021-11-11 19:13:06 -0500

    React Setup Complete

---
+++ server.js
@@ -0,0 +1,32 @@
+const express  = require( 'express' ),
+      app      = express()
+      path = require('path')
+
+const todos = [
+  { name:'buy groceries', completed:false }
+]
+
+app.use( express.json() )
+
+const publicDirectoryPath = path.join(__dirname, 'build/src')
+app.use(express.static(publicDirectoryPath))
+
+app.get('/', function(request, response) {
+    response.sendFile( __dirname + '/' )
+})
+
+app.get( '/read', ( req, res ) => res.json( todos ) )
+
+app.post( '/add', ( req,res ) => {
+  todos.push( req.body )
+  res.json( todos )
+})
+
+app.post( '/change', function( req,res ) {
+  const idx = todos.findIndex( v => v.name === req.body.name )
+  todos[ idx ].completed = req.body.completed
+  
+  res.sendStatus( 200 )
+})
+
+app.listen(process.env.PORT || 3000)
